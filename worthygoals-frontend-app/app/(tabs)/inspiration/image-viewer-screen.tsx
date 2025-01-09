94b2f7d6935a360581c52db48cce64a9cbae2906
saad189 <saad18910@hotmail.com>
2025-01-10 02:19:02 +0500

    UI improvement

--- worthygoals-frontend-app/app/(tabs)/inspiration/image-viewer-screen.tsx
+++ worthygoals-frontend-app/app/(tabs)/inspiration/image-viewer-screen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useRoute } from '@react-navigation/native';
+import { ParamListBase, useRoute } from '@react-navigation/native';
 import ImageViewerModalComponent from '@/components/Common/ImageViewer';
-import { Dimensions, StyleSheet } from 'react-native';
+import { Dimensions, Pressable, StyleSheet, View } from 'react-native';
 import { BlurView } from 'expo-blur';
 import BorderGradient from '@/components/Common/BorderGradient';
+import { useNavigation } from 'expo-router';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 const { width, height } = Dimensions.get('window');
 
@@ -13,37 +15,42 @@ const ImageViewerModal = () => {
     const containerWidth = 0.85 * width;
     const containerHeight = 0.7 * height;
     const borderWidth = 6;
+    const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
     return (
-        <BlurView intensity={15} style={styles.container}>
-            <BorderGradient
-                borderWidth={borderWidth}
-                colors={[borderColor, '#1F1F21']}
-                start={{ x: 1, y: 1 }}
-                end={{ x: 0, y: 1 }}
-                outerStyle={{
-                    justifyContent: 'center',
-                    width: containerWidth,
-                    height: containerHeight,
-                }}
-            >
-                <ImageViewerModalComponent
-                    imageUri={imageUri}
-                    tag={tag}
-                    extraStyles={{
-                        width: containerWidth - borderWidth * 2,
-                        height: '100%',
-                        resizeMode: 'cover',
-                        borderRadius: 12
+        <Pressable style={styles.container} onPress={navigation.goBack}>
+            <BlurView intensity={15} style={styles.container}>
+                <BorderGradient
+                    borderWidth={borderWidth}
+                    colors={[borderColor, '#1F1F21']}
+                    start={{ x: 1, y: 1 }}
+                    end={{ x: 0, y: 1 }}
+                    outerStyle={{
+                        marginTop: 90,
+                        justifyContent: 'center',
+                        width: containerWidth,
+                        height: containerHeight,
                     }}
-                />
-            </BorderGradient>
-        </BlurView>
+                >
+                    <ImageViewerModalComponent
+                        imageUri={imageUri}
+                        tag={tag}
+                        extraStyles={{
+                            width: containerWidth - borderWidth * 2,
+                            height: '100%',
+                            resizeMode: 'cover',
+                            borderRadius: 12
+                        }}
+                    />
+                </BorderGradient>
+            </BlurView>
+        </Pressable>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: 'rgba(0,0,0,0.5)',
